718a6dab8e1c0386a1d0315659a3486522de54ec
jodonghyeon3 <jawoo1003@gmail.com>
2024-04-14 15:48:32 +0900

    프로젝트 초기셋팅

---
+++ src/main/java/com/example/mdmggreal/MdmggRealApplication.java
@@ -0,0 +1,13 @@
+package com.example.mdmggreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmggRealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdmggRealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mdmggreal/MdmggRealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mdmggreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdmggRealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
