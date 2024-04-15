e0d39a277012896feefa3b7452d496b7474c55ee
choifriendship <choifriendship@naver.com>
2024-04-15 22:18:03 +0900

    cicdTest

---
+++ src/main/java/com/example/mdmggreal/testController.java
@@ -0,0 +1,13 @@
+package com.example.mdmggreal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @RequestMapping("/test")
+    public String main() {
+        return "cicdTest";
+    }
+}
