af91562812df19d00d0532445915f07691bbef64
choifriendship <choifriendship@naver.com>
2024-04-28 21:56:22 +0900

    testController update

--- src/main/java/com/example/mdmggreal/testController.java
+++ src/main/java/com/example/mdmggreal/testController.java
@@ -6,9 +6,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class testController {
 
-    @RequestMapping("/test")
-    public String main() {
-        return "cicdTest";
+    @Value("${logging-module.version}")
+    private String version;
+
+    @GetMapping("/")
+    public String version() {
+        return String.format("Project Version : %s", version);
+    }
+
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "healthy";
     }
 
 }
