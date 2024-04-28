b9d24b1c5593d93ddf463f5be5d38afd0eeb30ab
choifriendship <choifriendship@naver.com>
2024-04-28 22:03:52 +0900

    testController update

--- src/main/java/com/example/mdmggreal/testController.java
+++ src/main/java/com/example/mdmggreal/testController.java
@@ -1,5 +1,7 @@
 package com.example.mdmggreal;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
