59996abc176a997199566409721e87607e5ba7e6
choifriendship <choifriendship@naver.com>
2024-05-01 15:04:37 +0900

    application.tar.gpg add

--- src/main/java/com/example/mdmggreal/TestController.java
+++ src/main/java/com/example/mdmggreal/TestController.java
@@ -1,12 +1,12 @@
 package com.example.mdmggreal;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class testController {
+public class TestController {
 
     @Value("${logging-module.version}")
     private String version;
