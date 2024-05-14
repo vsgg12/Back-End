cf7f98888d0db6b311790d5508ce4d7251fb5e59
choifriendship <choifriendship@naver.com>
2024-05-14 22:39:23 +0900

    한달 지난 알람 삭제 배치 추가

--- src/main/java/com/example/mdmggreal/alarm/repository/AlarmRepository.java
+++ src/main/java/com/example/mdmggreal/alarm/repository/AlarmRepository.java
@@ -4,10 +4,13 @@ import com.example.mdmggreal.alarm.entity.Alarm;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
 
     List<Alarm> findByToken(String token);
+    // 한 달 전에 생성된 알람 삭제
+    void deleteByCreatedDateTimeBefore(LocalDateTime createdDateTime);
 }
