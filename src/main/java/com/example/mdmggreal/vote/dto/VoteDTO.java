99e25f3a662b3a8d0fcaac13b70eaad7aa5c38fe
choifriendship <choifriendship@naver.com>
2024-05-15 23:32:00 +0900

    투표 추가

---
+++ src/main/java/com/example/mdmggreal/vote/dto/VoteDTO.java
@@ -0,0 +1,13 @@
+package com.example.mdmggreal.vote.dto;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VoteDTO {
+
+    private Long ingameInfoId;
+    private Long ratio;
+}
---
+++ src/main/java/com/example/mdmggreal/vote/repository/VoteRepository.java
@@ -0,0 +1,12 @@
+package com.example.mdmggreal.vote.repository;
+
+import com.example.mdmggreal.vote.entity.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+
+
+
+}
