b252b75fa5134b2cb4a3a07a1d81d458dc04ce14
choifriendship <choifriendship@naver.com>
2024-05-18 19:41:25 +0900

    post content 따로 빼서 수정

--- src/main/java/com/example/mdmggreal/post/dto/request/PostAddRequest.java
+++ src/main/java/com/example/mdmggreal/post/dto/request/PostAddRequest.java
@@ -7,7 +7,7 @@ import com.example.mdmggreal.post.entity.type.VideoType;
 import java.util.List;
 public record PostAddRequest(
         String title,
-        String content,
+//        String content,
         VideoType type,
         List<String> hashtag,
         List<InGameInfoRequest> inGameInfoRequests,
