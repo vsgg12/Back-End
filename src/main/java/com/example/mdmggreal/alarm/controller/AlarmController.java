2176c52956a8e55a1d059bc23f789ad1dddfe40c
choifriendship <choifriendship@naver.com>
2024-05-18 17:40:51 +0900

    AlarmController 필요없는 코드 삭제 mapping 변경 MemberController RequestParam 수정

--- src/main/java/com/example/mdmggreal/alarm/controller/AlarmController.java
+++ src/main/java/com/example/mdmggreal/alarm/controller/AlarmController.java
@@ -1,11 +1,13 @@
 package com.example.mdmggreal.alarm.controller;
 
-import com.example.mdmggreal.alarm.dto.AlarmDTO;
 import com.example.mdmggreal.alarm.entity.Alarm;
 import com.example.mdmggreal.alarm.service.AlarmService;
 import com.example.mdmggreal.global.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -17,18 +19,9 @@ public class AlarmController {
     private final AlarmService alarmService;
 
     /*
-     * 테스트
-     */
-    @PostMapping("/save")
-    public void addAlarm(@RequestBody AlarmDTO alarmDTO, String mobile) {
-        // 테스트용
-        alarmService.saveAlarm(alarmDTO, mobile);
-    }
-
-    /*
      * 리스트 가져오기
      */
-    @GetMapping("/list")
+    @GetMapping("/users")
     public List<Alarm> getAlarmListForUser(@RequestHeader(value = "Authorization") String token) {
         JwtUtil.validateToken(token);
         String mobile = JwtUtil.getMobile(token);
