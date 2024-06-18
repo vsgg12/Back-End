2001b66445320141758c88fa4c50e75eff61ca45
jodonghyeon3 <jawoo1003@gmail.com>
2024-06-19 00:19:34 +0900

    test: cors문제 해결중

--- src/main/java/com/example/mdmggreal/global/config/WebConfig.java
+++ src/main/java/com/example/mdmggreal/global/config/WebConfig.java
@@ -10,8 +10,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")   // 허용할 출처
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://vsgg.co.kr",
+                        "https://www.vsgg.co.kr"
+                )   // 허용할 출처
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP method
+                .allowedHeaders("*")  // 모든 헤더 허용
+                .exposedHeaders("Authorization", "Content-Type")  // 노출할 헤더
                 .allowCredentials(true)  // 쿠키 인증 요청 허용
                 .maxAge(3600);  // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
