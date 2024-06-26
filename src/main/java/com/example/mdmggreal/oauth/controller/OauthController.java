1fb10980271aec0e2fce522dcc5c63945ab844b3
YesunPark <lynn08082@gmail.com>
2024-06-26 13:13:50 +0900

    Fix: 네이버 인가코드, 상태값 파라미터가 아니라 body로 받도록 수정, 백엔드용 테스트 controller 추가

--- src/main/java/com/example/mdmggreal/oauth/controller/OauthController.java
+++ src/main/java/com/example/mdmggreal/oauth/controller/OauthController.java
@@ -2,15 +2,13 @@ package com.example.mdmggreal.oauth.controller;
 
 import com.example.mdmggreal.oauth.NaverLoginParams;
 import com.example.mdmggreal.oauth.dto.NaverLoginURLResponse;
+import com.example.mdmggreal.oauth.dto.SignInRequest;
 import com.example.mdmggreal.oauth.dto.SignInResponse;
 import com.example.mdmggreal.oauth.service.NaverService;
 import com.example.mdmggreal.oauth.service.OAuthLoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
 
@@ -30,11 +28,21 @@ public class OauthController {
 
     // 네이버 인가코드, 상태값 받아서 로그인 시도
     @GetMapping("/signin")
-    public ResponseEntity<SignInResponse> naverSignin(@RequestParam String code, @RequestParam String state) {
+    public ResponseEntity<SignInResponse> naverSignin(@RequestBody SignInRequest request) {
         NaverLoginParams params = NaverLoginParams.builder()
-                .authorizationCode(code)
-                .state(state)
+                .authorizationCode(request.getCode())
+                .state(request.getState())
                 .build();
         return ResponseEntity.ok(oAuthLoginService.signIn(params));
     }
+
+// 네이버 인가코드, 상태값 받아서 로그인 시도 - 프론트 없이 테스트할 때 사용
+//    @GetMapping("/callback/naver")
+//    public ResponseEntity<SignInResponse> naverSigninTest(@RequestParam String code, @RequestParam String state) {
+//        NaverLoginParams params = NaverLoginParams.builder()
+//                .authorizationCode(code)
+//                .state(state)
+//                .build();
+//        return ResponseEntity.ok(oAuthLoginService.signIn(params));
+//    }
 }
