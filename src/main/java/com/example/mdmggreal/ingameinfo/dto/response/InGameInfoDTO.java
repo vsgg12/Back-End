11f77825807d13a3e32aee7ee14521b137f0101b
jodonghyeon <jodonghyeon3@bubblecon.io>
2024-07-03 16:14:17 +0900

    Fix: InGameInfoResponse 네이밍 변경, InGameInfoDTO에 averageRatio 필드추가

--- src/main/java/com/example/mdmggreal/ingameinfo/dto/response/InGameInfoDTO.java
+++ src/main/java/com/example/mdmggreal/ingameinfo/dto/response/InGameInfoDTO.java
@@ -1,26 +1,27 @@
 package com.example.mdmggreal.ingameinfo.dto.response;
 
 import com.example.mdmggreal.ingameinfo.entity.InGameInfo;
-import com.example.mdmggreal.ingameinfo.type.Position;
-import com.example.mdmggreal.ingameinfo.type.Tier;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder
-public class InGameInfoResponse {
+public class InGameInfoDTO {
 
     private Long inGameInfoId;
     private String tier;
     private String position;
     private String championName;
+    private Double averageRatio;
 
-    public static InGameInfoResponse of(InGameInfo inGameInfo) {
-        return InGameInfoResponse.builder()
+
+    public static InGameInfoDTO of(InGameInfo inGameInfo) {
+        return InGameInfoDTO.builder()
                 .inGameInfoId(inGameInfo.getId())
                 .tier(inGameInfo.getTier().getName())
                 .position(inGameInfo.getPosition().getName())
                 .championName(inGameInfo.getChampionName())
+                .averageRatio(inGameInfo.getAverageRatio())
                 .build();
     }
 
