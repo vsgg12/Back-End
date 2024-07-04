a717cd8bfc9500ee1a712674d0a047f680baa730
YesunPark <lynn08082@gmail.com>
2024-07-04 09:57:06 +0900

    Fix: 에러응답 형식 BaseResponse로 통일

--- src/main/java/com/example/mdmggreal/comment/controller/CommentController.java
+++ src/main/java/com/example/mdmggreal/comment/controller/CommentController.java
@@ -8,11 +8,12 @@ import com.example.mdmggreal.global.response.BaseResponse;
 import com.example.mdmggreal.global.security.JwtUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RequestMapping("/api/post/{postid}/comment")
 @RequiredArgsConstructor
@@ -28,20 +29,20 @@ public class CommentController {
     ) {
         Long memberId = JwtUtil.getMemberId(token);
         commentService.addComment(postId, request, memberId);
-        return ResponseEntity.ok(BaseResponse.from(HttpStatus.CREATED));
+        return BaseResponse.toResponseEntity(CREATED);
     }
 
     @GetMapping
     public ResponseEntity<CommentGetListResponse> commentGetList(@PathVariable(value = "postid") Long postId) {
         List<CommentDTO> commentList = commentService.getCommentList(postId);
-        return ResponseEntity.ok(CommentGetListResponse.from(commentList, HttpStatus.OK));
+        return ResponseEntity.ok(CommentGetListResponse.from(commentList, OK));
     }
 
     @DeleteMapping("/{commentid}")
     public ResponseEntity<BaseResponse> commentDelete(@RequestHeader(value = "Authorization") String token, @PathVariable(value = "postid") Long postId, @PathVariable(value = "comment") Long commentId) {
         Long memberId = JwtUtil.getMemberId(token);
         commentService.deleteCommentList(postId, memberId, commentId);
-        return ResponseEntity.ok(BaseResponse.from(HttpStatus.OK));
+        return BaseResponse.toResponseEntity(OK);
     }
 
 }
--- src/main/java/com/example/mdmggreal/global/response/BaseResponse.java
+++ src/main/java/com/example/mdmggreal/global/response/BaseResponse.java
@@ -1,21 +1,38 @@
 package com.example.mdmggreal.global.response;
 
+import com.example.mdmggreal.global.exception.ErrorCode;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Getter
 @SuperBuilder
-@Setter
 public class BaseResponse {
     private Integer resultCode;
     private String resultMsg;
 
-    public static BaseResponse from(HttpStatus httpStatus) {
+    public static BaseResponse from(ErrorCode e) {
         return BaseResponse.builder()
-                .resultCode(httpStatus.value())
-                .resultMsg(httpStatus.name())
+                .resultCode(e.getHttpStatus().value())
+                .resultMsg(e.getMessage())
                 .build();
     }
+
+    public static ResponseEntity<BaseResponse> toResponseEntity(HttpStatus httpStatus) {
+        return ResponseEntity.status(httpStatus)
+                .body(BaseResponse.builder()
+                        .resultCode(httpStatus.value())
+                        .resultMsg(httpStatus.name())
+                        .build());
+    }
+
+    public static ResponseEntity<BaseResponse> toResponseEntity(ErrorCode e) {
+        return ResponseEntity.status(e.getHttpStatus())
+                .body(BaseResponse.builder()
+                        .resultCode(e.getHttpStatus().value())
+                        .resultMsg(e.getMessage())
+                        .build());
+    }
+
 }
