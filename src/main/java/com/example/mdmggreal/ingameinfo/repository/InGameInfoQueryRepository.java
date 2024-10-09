ad0cc0c61fbfdacc9a97f6b14610408b71a7d7da
jodonghyeon3 <jawoo1003@gmail.com>
2024-10-09 21:36:26 +0900

    Fix: Post Batch 수정

--- src/main/java/com/example/mdmggreal/ingameinfo/repository/InGameInfoQueryRepository.java
+++ src/main/java/com/example/mdmggreal/ingameinfo/repository/InGameInfoQueryRepository.java
@@ -1,14 +1,6 @@
 package com.example.mdmggreal.ingameinfo.repository;
 
-import com.example.mdmggreal.ingameinfo.dto.response.InGameInfoDTO;
-import com.example.mdmggreal.ingameinfo.dto.response.QInGameInfoDTO;
 import com.example.mdmggreal.ingameinfo.entity.InGameInfo;
-import com.example.mdmggreal.ingameinfo.entity.QInGameInfo;
-import com.example.mdmggreal.vote.entity.QVote;
-import com.querydsl.core.types.ExpressionUtils;
-import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.core.types.dsl.NumberPath;
-import com.querydsl.jpa.JPAExpressions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
@@ -20,36 +12,17 @@ import static com.example.mdmggreal.vote.entity.QVote.vote;
 @Slf4j
 public class InGameInfoQueryRepository extends QuerydslRepositorySupport {
 
-
     public InGameInfoQueryRepository() {
         super(InGameInfo.class);
     }
 
-    public InGameInfoDTO getInGameInfoVoteMaxRatioByPostId(Long postId) {
-        QInGameInfo inGameInfo = QInGameInfo.inGameInfo;
-        QVote vote = QVote.vote;
 
-        NumberPath<Double> averageRatio = Expressions.numberPath(Double.class, "averageRatio");
-
-        QInGameInfoDTO qInGameInfoDTO = new QInGameInfoDTO(
-                inGameInfo.id,
-                inGameInfo.inGameTier,
-                inGameInfo.position,
-                inGameInfo.championName,
-                ExpressionUtils.as(
-                        JPAExpressions
-                                .select((vote.ratio.avg()).max())
-                                .from(vote)
-                                .where(vote.inGameInfo.id.eq(inGameInfo.id))
-                                .groupBy(vote.inGameInfo.id),
-                        "averageRatio"));
-
-
+    public InGameInfo getInGameInfoVoteMaxRatioByPostId(Long postId) {
         return from(inGameInfo)
-                .select(qInGameInfoDTO)
                 .leftJoin(vote).on(inGameInfo.id.eq(vote.inGameInfo.id))
                 .where(inGameInfo.post.id.eq(postId))
-                .fetchOne();
+                .orderBy(inGameInfo.totalRatio.desc())
+                .fetchFirst();
     }
 
     public Double getAverageRatioByPostId(Long inGameInfoId) {
