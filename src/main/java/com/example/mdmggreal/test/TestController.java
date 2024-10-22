0bec2a107a7fb3d3fde7acab1034903bacb3311d
박예선 <yesun.prk@danbiedu.co.kr>
2024-10-22 16:54:03 +0900

    Refactor: 테스트 컨트롤러 롤백

--- src/main/java/com/example/mdmggreal/test/TestController.java
+++ src/main/java/com/example/mdmggreal/test/TestController.java
@@ -1,6 +1,5 @@
 package com.example.mdmggreal.test;
 
-import com.example.mdmggreal.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,15 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TestController {
 
-    private final PostService postService;
-
     @PostMapping("/content")
     public String test(String content) {
         return "또다시 테스트~~!";
     }
     @GetMapping
     public ResponseEntity test() {
-        postService.deletePost(63L, 13L);
         return ResponseEntity.ok().build();
     }
 }
